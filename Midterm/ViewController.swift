c76452c19eb199762c6c7efdf1acf870028b5346
Chrybear <50463991+Chrybear@users.noreply.github.com>
2021-03-06 19:21:51 -0500

    Created course class

--- Midterm/ViewController.swift
+++ Midterm/ViewController.swift
@@ -7,6 +7,26 @@
 
 import UIKit
 
+//Class for each course type
+
+class course{
+    var title = [String]() //Value to hold each class' title
+    var lectureImg = [UIImage]() //Images for each class
+    var challenge = [Int]() //Challenge level for each course
+    
+    //Constructor
+    init(title: [String], lectureImg: [UIImage], challenge: [Int]) {
+        self.title = title
+        self.lectureImg = lectureImg
+        self.challenge = challenge
+    }
+    
+    //Default constructor
+    init() {
+        print("Lovely weather.")
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
